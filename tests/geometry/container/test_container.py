90a0348b24f649b64f52387964c441763aff6a14
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-10 15:12:20 +0000

    test: add tests to Container class

---
+++ tests/geometry/container/test_container.py
@@ -0,0 +1,21 @@
+import pathlib
+from src.geometry.container import Container
+from src.geometry.rectImage import RectImage
+from src.utils.image import load
+
+
+currentPath = pathlib.Path(__file__).parent.resolve()
+
+
+def test_should_method_isMaximized_return_True_when_battleList_is_maximized():
+    image = load(f'{currentPath}/battleListMaximized.png')
+    rectImage = RectImage(0, 0, image)
+    container = Container(rectImage)
+    assert container.isMaximized() == True
+
+
+def test_should_method_isMaximized_return_False_when_battleList_is_minimized():
+    image = load(f'{currentPath}/battleListMinimized.png')
+    rectImage = RectImage(0, 0, image)
+    container = Container(rectImage)
+    assert container.isMaximized() == False
