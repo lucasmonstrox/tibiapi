3e022e52119d65c7dbfe6d3f8a95a1b39c09705b
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-10 12:53:04 +0000

    feat: begin project

---
+++ src/geometry/bbox.py
@@ -0,0 +1,11 @@
+class BBox:
+    x: int
+    y: int
+    width: int
+    height: int
+
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
