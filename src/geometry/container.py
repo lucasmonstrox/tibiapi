6580afcaacb3f222f6d8d3b62f42d21107a09a9f
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-10 16:17:12 +0000

    docs: update doc blocks

--- src/geometry/container.py
+++ src/geometry/container.py
@@ -8,8 +8,22 @@ class Container:
         self.rectImage = rectImage
 
     def getCloseButtonRectImage(self) -> RectImage:
-        return RectImage(161, 2, self.rectImage.image[2:14, 161:173])
+        """
+        Retrieve the bounding rectangle image of the close button in a specific container.
+        """
+        return RectImage(self.rectImage.x + 161, self.rectImage.y + 2, self.rectImage.image[2:14, 161:173])
+
+    def getMaximizeOrMinimizeButtonRectImage(self) -> RectImage:
+        """
+        Retrieve the bounding rectangle image of the maximize or minimize button.
+        """
+        return RectImage(self.rectImage.x + 161, self.rectImage.y + 2, self.rectImage.image[2:14, 161:173])
 
     def isMaximized(self) -> bool:
+        """
+        Check whether the container is maximized or minimized.
+
+        It will return True when container is maximized or False if it is minimized.
+        """
         pixel = self.rectImage.image[5, 154]
         return pixel[0] != 192 and pixel[1] != 192 and pixel[2] != 192
