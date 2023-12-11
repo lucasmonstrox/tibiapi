29e9262b443013f87e755ac80561484a84a2c276
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-11 19:47:36 +0000

    feat: add save method

--- src/utils/image.py
+++ src/utils/image.py
@@ -1,8 +1,15 @@
 import cv2
 import numpy as np
+from PIL import Image
 
 
 # TODO: add return type
 def load(path: str):
     bgraImage = cv2.imread(path)
     return np.array(bgraImage, dtype=np.uint8)
+
+
+# TODO: add parameter type
+def save(arr, name: str):
+    im = Image.fromarray(arr)
+    im.save(name)
