5d8174800e923cd65b83183531d5caeebc265aaa
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-11 23:14:37 +0000

    refactor: add missing types

--- src/geometry/rectImage.py
+++ src/geometry/rectImage.py
@@ -1,10 +1,12 @@
+import numpy as np
+
+
 class RectImage:
     x: int
     y: int
     width: int
     height: int
-    # TODO: add type
-    image: any
+    image: np.ndarray
 
     def __init__(self, x: int, y: int, image):
         self.x = x
@@ -13,7 +15,6 @@ class RectImage:
         self.height = len(image)
         self.image = image
 
-    # TODO: add parameter type
     @staticmethod
-    def makeFromRectImage(rectImage, x: int, y: int, width: int, height: int):
+    def makeFromRectImage(rectImage: np.ndarray, x: int, y: int, width: int, height: int):
         return RectImage(rectImage.x + x, rectImage.y + y, rectImage.image[y:y + height, x: x + width])
