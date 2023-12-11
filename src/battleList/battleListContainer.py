a6d9ee0722f0210b84f53fdc9be2f8df483de1a9
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-11 23:44:55 +0000

    feat: add property hidePlayersButton and check if is enabled

--- src/battleList/battleListContainer.py
+++ src/battleList/battleListContainer.py
@@ -14,6 +14,21 @@ class BattleListContainer:
         return RectImage.makeFromRectImage(self.container.rectImage, 133, 2, 12, 12)
 
     @cached_property
+    def hidePlayersButton(self) -> RectImage | None:
+        if not self.container.isMaximized:
+            return None
+        if not self.isConfiguringCreatures:
+            return None
+        return RectImage.makeFromRectImage(self.container.rectImage, 23, 17, 20, 20)
+
+    @cached_property
+    def hidePlayersButtonIsEnabled(self) -> RectImage | None:
+        if self.hidePlayersButton is None:
+            return False
+        pixel = self.hidePlayersButton.image[0, 0]
+        return pixel[0] == 41 and pixel[1] == 41 and pixel[2] == 41
+
+    @cached_property
     def isConfiguringCreatures(self) -> bool:
         pixel = self.configureCreaturesButton.image[0, 0]
         return pixel[0] == 28 and pixel[1] == 28 and pixel[2] == 28
