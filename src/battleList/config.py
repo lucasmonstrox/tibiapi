de93ec19860b22db7ea47f6ca4b110ca86dcb082
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-13 02:34:42 +0000

    feat: add creatures names hashes

---
+++ src/battleList/config.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pathlib
+from src.utils.image import hashit, load
+from src.wiki.creatures import creatures
+
+
+parentPath = pathlib.Path(__file__).parent.resolve()
+imagesPath = f'{parentPath}/images'
+monstersPath = f'{imagesPath}/monsters'
+creaturesNamesImagesHashes = {}
+
+for creatureName in creatures:
+    creatureNameImage = load(
+        f'{monstersPath}/{creatureName}.png')
+    creatureNameImage = np.ravel(creatureNameImage[8:9, 0:115])
+    creatureNameImageHash = hashit(creatureNameImage)
+    creaturesNamesImagesHashes[creatureNameImageHash] = creatureName
