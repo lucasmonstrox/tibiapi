284d1d2abbbc4a991f9221020b07c3b87dfe90f4
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-21 13:03:48 +0000

    refactor: add missing type to battleList Creature

--- src/battleList/creature.py
+++ src/battleList/creature.py
@@ -3,7 +3,7 @@ class Creature:
     healthPercentage: int
     isBeingAttacked: bool
 
-    def __init__(self, name: str, healthPercentage: int = 100, isBeingAttacked=False):
+    def __init__(self, name: str, healthPercentage: int = 100, isBeingAttacked: bool = False):
         self.name = name
         self.healthPercentage = healthPercentage
         self.isBeingAttacked = isBeingAttacked
