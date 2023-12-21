b9df99f61ece0e9935297a732069e1235e301f2b
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-21 10:40:08 +0000

    test: fix wrong tests

--- tests/battleList/test_hidePlayersButton.py
+++ tests/battleList/test_hidePlayersButton.py
@@ -14,30 +14,40 @@ def test_should_return_None_when_battle_list_is_minimized():
     battleList = BattleList(battleListRectImage)
     assert battleList.togglePlayersButton is None
 
+
 def test_should_return_None_when_not_configuring_creatures():
-    battleListImage = load(f'{currentPath}/images/battleListNotConfiguringCreatures.png')
+    battleListImage = load(
+        f'{currentPath}/images/battleListNotConfiguringCreatures.png')
     battleListRectImage = RectImage(0, 0, battleListImage)
     battleList = BattleList(battleListRectImage)
     assert battleList.togglePlayersButton is None
 
+
 def test_should_return_hidePlayersButton_when_configuring_creatures():
-    battleListImage = load(f'{currentPath}/images/battleListConfiguringCreatures.png')
+    battleListImage = load(
+        f'{currentPath}/images/battleListConfiguringCreatures.png')
     battleListRectImage = RectImage(0, 0, battleListImage)
     battleList = BattleList(battleListRectImage)
-    hidePlayersButtonImage = load('src/battleList/images/buttons/hidePlayers.png')
+    hidePlayersButtonImage = load(
+        'src/battleList/images/buttons/hidePlayers.png')
     assert battleList.togglePlayersButton.x == 23
     assert battleList.togglePlayersButton.y == 17
     assert battleList.togglePlayersButton.width == 20
     assert battleList.togglePlayersButton.height == 20
-    np.testing.assert_array_equal(battleList.togglePlayersButton.image, hidePlayersButtonImage)
+    np.testing.assert_array_equal(
+        battleList.togglePlayersButton.image, hidePlayersButtonImage)
+
 
 def test_should_return_showPlayersButton_when_configuring_creatures():
-    battleListImage = load(f'{currentPath}/images/battleListConfigureCreaturesWithButtonsNotPressed.png')
+    battleListImage = load(
+        f'{currentPath}/images/battleListConfigureCreaturesWithButtonsNotPressed.png')
     battleListRectImage = RectImage(0, 0, battleListImage)
     battleList = BattleList(battleListRectImage)
-    showPlayersButtonImage = load('src/battleList/images/buttons/showPlayers.png')
+    showPlayersButtonImage = load(
+        'src/battleList/images/buttons/showPlayers.png')
     assert battleList.togglePlayersButton.x == 23
     assert battleList.togglePlayersButton.y == 17
     assert battleList.togglePlayersButton.width == 20
     assert battleList.togglePlayersButton.height == 20
-    np.testing.assert_array_equal(battleList.togglePlayersButton.image, showPlayersButtonImage)
+    np.testing.assert_array_equal(
+        battleList.togglePlayersButton.image, showPlayersButtonImage)
---
+++ tests/battleList/topBar/test_configureCreaturesButton.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pathlib
+from src._common.rectImage import RectImage
+from src.battleList import TopBar
+from src.utils.image import load
+
+
+currentPath = pathlib.Path(__file__).parent.parent.resolve()
+
+
+def test_should_match_configure_creatures_button_image():
+    battleListImage = load(
+        f'{currentPath}/images/battleListNotConfiguringCreatures.png')
+    battleListRectImage = RectImage(0, 0, battleListImage)
+    topBar = TopBar(battleListRectImage)
+    configureCreaturesButton = load(
+        'src/battleList/images/buttons/configureCreatures.png')
+    assert topBar.configureCreaturesButton.x == 133
+    assert topBar.configureCreaturesButton.y == 2
+    assert topBar.configureCreaturesButton.width == 12
+    assert topBar.configureCreaturesButton.height == 12
+    np.testing.assert_array_equal(
+        topBar.configureCreaturesButton.image, configureCreaturesButton)
+
+
+def test_should_match_configure_creatures_button_pressed_image():
+    battleListImage = load(
+        f'{currentPath}/images/battleListConfiguringCreatures.png')
+    battleListRectImage = RectImage(0, 0, battleListImage)
+    topBar = TopBar(battleListRectImage)
+    configureCreaturesButton = load(
+        'src/battleList/images/buttons/configureCreaturesPressed.png')
+    assert topBar.configureCreaturesButton.x == 133
+    assert topBar.configureCreaturesButton.y == 2
+    assert topBar.configureCreaturesButton.width == 12
+    assert topBar.configureCreaturesButton.height == 12
+    np.testing.assert_array_equal(
+        topBar.configureCreaturesButton.image, configureCreaturesButton)
