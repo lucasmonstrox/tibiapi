573e537108ecbd55b469c51473003530d1d693de
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-21 10:29:34 +0000

    feat: add locate function

--- src/utils/image.py
+++ src/utils/image.py
@@ -13,6 +13,14 @@ def load(path: str) -> np.ndarray:
     return np.ascontiguousarray(bgraImage, dtype=np.uint8)
 
 
+def locate(compareImage: np.ndarray, img: np.ndarray, confidence: float = 0.85) -> tuple[int, int, int, int] | None:
+    match = cv2.matchTemplate(compareImage, img, cv2.TM_CCOEFF_NORMED)
+    res = cv2.minMaxLoc(match)
+    if res[1] <= confidence:
+        return None
+    return res[3][0], res[3][1], len(img[0]), len(img)
+
+
 def save(arr: np.ndarray, name: str):
     im = Image.fromarray(arr)
     im.save(name)
