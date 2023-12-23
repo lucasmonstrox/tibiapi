3a26d59ee17a6962a6464bc4ee4bc849e36ae8f6
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-23 11:10:17 +0000

    feat: add func to get mana percentage

--- src/statusBar/statusBar.py
+++ src/statusBar/statusBar.py
@@ -1,6 +1,6 @@
 from functools import cached_property
 from src._common.rectImage import RectImage
-from .utils import healthPercentage
+from .utils import healthPercentage, manaPercentage
 
 
 class StatusBar:
@@ -10,3 +10,7 @@ class StatusBar:
     @cached_property
     def healthPercentage(self) -> int:
         return healthPercentage(self.rectImage.image[5, 14:])
+
+    @cached_property
+    def manaPercentage(self) -> int:
+        return manaPercentage(self.rectImage.image[18, 14:])
