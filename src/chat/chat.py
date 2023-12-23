f00d22a7a6aa96d7300245cea932f9a854dfd5a1
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-23 12:26:15 +0000

    feat: add Chat/Tab class

---
+++ src/chat/chat.py
@@ -0,0 +1,9 @@
+from .tab import Tab
+
+
+class Chat:
+    def getTabByIndex(self, index: int) -> Tab | None:
+        return None
+
+    def getTabByName(self, name: str) -> Tab | None:
+        return None
