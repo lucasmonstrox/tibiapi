6636f84595f011ffcd25ee8c30eb81c89dbe18be
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-23 18:18:43 +0000

    feat: improve Creature signature

--- src/gameWindow/creature.py
+++ src/gameWindow/creature.py
@@ -2,6 +2,7 @@ class Creature:
     name: str
     healthPercentage: int
     isBeingAttacked: bool
+    isUnderRoof: bool
     coordinate: tuple[int, int, int]
     slot: tuple[int, int]
     windowCoordinate: tuple[int, int]
