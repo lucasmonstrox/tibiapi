370cfaa14edfd8a3fe08a686b6f5830484f63aac
lucasmonstro <joao.galiano.silva@gmail.com>
2023-12-24 11:08:49 +0000

    feat: add scroll/resize signatures

---
+++ src/_common/container/resizeBehaviour.py
@@ -0,0 +1,15 @@
+class ResizeBehaviour:
+    def contract(self):
+        pass
+
+    def expand(self):
+        pass
+
+    def toHeight(self, height: int):
+        pass
+
+    def toItem(self, index: int = 0):
+        pass
+
+    def toLine(self, line: int):
+        pass
---
+++ src/_common/container/scrollBehaviour.py
@@ -0,0 +1,12 @@
+class ScrollBehaviour:
+    def toTop(self):
+        pass
+
+    def toBottom(self):
+        pass
+
+    def toItem(self, index: int = 0):
+        pass
+
+    def toLine(self, line: int):
+        pass
