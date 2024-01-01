79d7b7c73876edcdf8c8f3b3c8d71dac37566692
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-01 06:31:59 +0000

    refactor: improve getCreaturesNamesImages performance

--- src/battleList/utils.py
+++ src/battleList/utils.py
@@ -2,15 +2,13 @@ from numba import njit
 import numpy as np
 
 
-@njit(cache=True, fastmath=True)
+@njit(cache=True, fastmath=True, boundscheck=False)
 def getCreaturesNamesImages(content: np.ndarray, filledSlotsCount: int) -> np.ndarray:
-    creaturesNamesImages = np.ascontiguousarray(
-        np.zeros((filledSlotsCount, 115), dtype=np.uint8))
-    for i in range(filledSlotsCount):
-        y = 11 + (i * 22)
-        for j, value in enumerate(content[y, 23:138]):
+    creaturesNamesImages = np.zeros((filledSlotsCount, 115), dtype=np.uint8)
+    for y in range(filledSlotsCount):
+        for x, value in enumerate(content[11 + (y * 22), 23:138]):
             if value == 192 or value == 247:
-                creaturesNamesImages[i, j] = 192
+                creaturesNamesImages[y, x] = 192
     return creaturesNamesImages
 
 
