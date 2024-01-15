bfa5b79eff89082a1438b81a314e54ae7df1730d
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-15 18:18:09 +0000

    refactor: add missing type

--- src/_common/rectImage/rectImage.py
+++ src/_common/rectImage/rectImage.py
@@ -8,7 +8,7 @@ class RectImage:
     height: int
     image: Image
 
-    def __init__(self, x: int, y: int, image):
+    def __init__(self, x: int, y: int, image: Image):
         self.x = x
         self.y = y
         self.width = len(image[0])
