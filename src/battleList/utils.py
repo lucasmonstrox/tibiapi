c33d67814379958f5be477eb5085425063b12a03
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-15 18:17:52 +0000

    refactor: reorganize order of functions

--- src/battleList/utils.py
+++ src/battleList/utils.py
@@ -3,16 +3,6 @@ import numpy as np
 from src._common.typings import GrayImage, Image
 
 
-@njit(cache=True, fastmath=True, boundscheck=False)
-def getCreaturesNamesImages(content: Image, filledSlotsCount: int) -> GrayImage:
-    creaturesNamesImages = np.zeros((filledSlotsCount, 115), dtype=np.uint8)
-    for y in range(filledSlotsCount):
-        for x, value in enumerate(content[11 + (y * 22), 23:138]):
-            if value == 192 or value == 247:
-                creaturesNamesImages[y, x] = 192
-    return creaturesNamesImages
-
-
 @njit(cache=True, fastmath=True)
 def creaturesCount(innerContent: Image) -> int:
     creaturesCount = 0
@@ -23,3 +13,13 @@ def creaturesCount(innerContent: Image) -> int:
             break
         creaturesCount += 1
     return creaturesCount
+
+
+@njit(cache=True, fastmath=True, boundscheck=False)
+def getCreaturesNamesImages(content: Image, filledSlotsCount: int) -> GrayImage:
+    creaturesNamesImages = np.zeros((filledSlotsCount, 115), dtype=np.uint8)
+    for y in range(filledSlotsCount):
+        for x, value in enumerate(content[11 + (y * 22), 23:138]):
+            if value == 192 or value == 247:
+                creaturesNamesImages[y, x] = 192
+    return creaturesNamesImages
