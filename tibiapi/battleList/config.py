3a0a08c95c0754c4ba515164204ae586e1f43890
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-19 20:20:49 +0000

    refactor: improve code

--- tibiapi/battleList/config.py
+++ tibiapi/battleList/config.py
@@ -4,19 +4,14 @@ from tibiapi.utils.image import hashit, load
 from tibiapi.wiki.creatures import creatures
 
 
-print('__file__', __file__)
-print('os.path.abspath(__file__)', os.path.abspath(__file__))
-print('os.path.dirname(os.path.abspath(__file__))',
-      os.path.dirname(os.path.abspath(__file__)))
 parentPath = os.path.dirname(os.path.abspath(__file__))
 imagesPath = f'{parentPath}/images'
-monstersPath = f'{imagesPath}/monsters'
+creaturesPath = f'{imagesPath}/monsters'
 creaturesNamesImagesHashes = {}
 
 for creatureName in creatures:
-    creaturePath = f'{monstersPath}/{creatureName}.png'
-    print('creaturePath', creaturePath)
-    creatureNameImage = load(creaturePath)
+    creatureNameImagePath = f'{creaturesPath}/{creatureName}.png'
+    creatureNameImage = load(creatureNameImagePath)
     creatureNameImage = np.ravel(creatureNameImage[8:9, 0:115][:, :, 0])
     creatureNameImageHash = hashit(creatureNameImage)
     creaturesNamesImagesHashes[creatureNameImageHash] = creatureName
