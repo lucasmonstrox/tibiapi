b740b8fb171eec003fc85c3e9f243470ee363b94
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-19 19:53:05 +0000

    refactor: test paths

--- tibiapi/battleList/config.py
+++ tibiapi/battleList/config.py
@@ -4,6 +4,10 @@ from tibiapi.utils.image import hashit, load
 from tibiapi.wiki.creatures import creatures
 
 
+print('__file__', __file__)
+print('os.path.abspath(__file__)', os.path.abspath(__file__))
+print('os.path.dirname(os.path.abspath(__file__))',
+      os.path.dirname(os.path.abspath(__file__)))
 parentPath = os.path.dirname(os.path.abspath(__file__))
 imagesPath = f'{parentPath}/images'
 monstersPath = f'{imagesPath}/monsters'
