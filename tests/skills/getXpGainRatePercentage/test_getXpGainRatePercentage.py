3905bc377f971946893b34029a31a5f952b96654
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-20 17:17:32 +0000

    refactor: rename method

--- tests/skills/getXpGainRatePercentage/test_getXpGainRatePercentage.py
+++ tests/skills/getXpGainRatePercentage/test_getXpGainRatePercentage.py
@@ -13,24 +13,24 @@ def loadSkillsByImage(imagePath: str) -> Skills:
 
 def test_should_return_None_when_skills_container_is_minimized():
     skills = loadSkillsByImage('skillsMinimized.png')
-    assert skills.getXpGainRate() is None
+    assert skills.getXpGainRatePercentage() is None
 
 
 def test_should_return_None_when_skills_xp_gain_rate_label_is_not_visible():
     skills = loadSkillsByImage('xpGainRateLabelMinimized.png')
-    assert skills.getXpGainRate() is None
+    assert skills.getXpGainRatePercentage() is None
 
 
 def test_should_return_50_when_pixels_are_orange():
     skills = loadSkillsByImage('xpGainRateWith50Percentage.png')
-    assert skills.getXpGainRate() == 50
+    assert skills.getXpGainRatePercentage() == 50
 
 
 def test_should_return_100_when_pixels_are_gray():
     skills = loadSkillsByImage('xpGainRateWith100Percentage.png')
-    assert skills.getXpGainRate() == 100
+    assert skills.getXpGainRatePercentage() == 100
 
 
 def test_should_return_150_when_pixels_are_green():
     skills = loadSkillsByImage('xpGainRateWith150Percentage.png')
-    assert skills.getXpGainRate() == 150
+    assert skills.getXpGainRatePercentage() == 150
