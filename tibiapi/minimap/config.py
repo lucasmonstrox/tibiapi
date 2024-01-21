56281473ac1b63720d36e91dca56f7dea287900a
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-21 14:48:24 +0000

    refactor: remove redundant paths

--- tibiapi/minimap/config.py
+++ tibiapi/minimap/config.py
@@ -3,28 +3,31 @@ from tibiapi.utils.image import hashit, load
 
 
 currentPath = pathlib.Path(__file__).parent.resolve()
+imagesFolder = f'{currentPath}/images'
+buttonsImagesFolder = f'{imagesFolder}/buttons'
+floorsLevelsImagesFolder = f'{imagesFolder}/floorsLevels'
 floors = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 images = {
     'buttons': {
-        'radarTools': load(f'{currentPath}/images/buttons/radarTools.png'),
+        'radarTools': load(f'{buttonsImagesFolder}/radarTools.png'),
     },
     'floorsLevels': {
-        0: load(f'{currentPath}/images/floorsLevels/0.png'),
-        1: load(f'{currentPath}/images/floorsLevels/1.png'),
-        2: load(f'{currentPath}/images/floorsLevels/2.png'),
-        3: load(f'{currentPath}/images/floorsLevels/3.png'),
-        4: load(f'{currentPath}/images/floorsLevels/4.png'),
-        5: load(f'{currentPath}/images/floorsLevels/5.png'),
-        6: load(f'{currentPath}/images/floorsLevels/6.png'),
-        7: load(f'{currentPath}/images/floorsLevels/7.png'),
-        8: load(f'{currentPath}/images/floorsLevels/8.png'),
-        9: load(f'{currentPath}/images/floorsLevels/9.png'),
-        10: load(f'{currentPath}/images/floorsLevels/10.png'),
-        11: load(f'{currentPath}/images/floorsLevels/11.png'),
-        12: load(f'{currentPath}/images/floorsLevels/12.png'),
-        13: load(f'{currentPath}/images/floorsLevels/13.png'),
-        14: load(f'{currentPath}/images/floorsLevels/14.png'),
-        15: load(f'{currentPath}/images/floorsLevels/15.png'),
+        0: load(f'{floorsLevelsImagesFolder}/0.png'),
+        1: load(f'{floorsLevelsImagesFolder}/1.png'),
+        2: load(f'{floorsLevelsImagesFolder}/2.png'),
+        3: load(f'{floorsLevelsImagesFolder}/3.png'),
+        4: load(f'{floorsLevelsImagesFolder}/4.png'),
+        5: load(f'{floorsLevelsImagesFolder}/5.png'),
+        6: load(f'{floorsLevelsImagesFolder}/6.png'),
+        7: load(f'{floorsLevelsImagesFolder}/7.png'),
+        8: load(f'{floorsLevelsImagesFolder}/8.png'),
+        9: load(f'{floorsLevelsImagesFolder}/9.png'),
+        10: load(f'{floorsLevelsImagesFolder}/10.png'),
+        11: load(f'{floorsLevelsImagesFolder}/11.png'),
+        12: load(f'{floorsLevelsImagesFolder}/12.png'),
+        13: load(f'{floorsLevelsImagesFolder}/13.png'),
+        14: load(f'{floorsLevelsImagesFolder}/14.png'),
+        15: load(f'{floorsLevelsImagesFolder}/15.png'),
     }
 }
 floorsLevelsImagesHashes = {}
