a1d4da1c462d70f80b96d7a8c93d66de2b1f8b04
lucasmonstro <joao.galiano.silva@gmail.com>
2024-01-22 18:39:26 +0000

    refactor: remove unused method

--- tibiapi/minimap/utils.py
+++ tibiapi/minimap/utils.py
@@ -14,8 +14,3 @@ def getCoordinateFromPixel(pixelCoordinate: PixelCoordinate) -> Coordinate:
 @njit(cache=True)
 def getPixelFromCoordinate(coordinate: Coordinate) -> PixelCoordinate:
     return coordinate[0] - (31744 - 106), coordinate[1] - (30976 - 109)
-
-
-@cacheObjectPosition
-def getRadarToolsPosition(screenshot: Image) -> Optional[BBox]:
-    return locate(screenshot, images['buttons']['radarTools'])
